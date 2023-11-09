8646e200c2c5f357c176e5bc05c539392c1ccd85
isalim079 <salimreza1025@gmail.com>
2023-11-09 13:31:04 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+// const jwt = require('jsonwebtoken')
+const cookieParser = require("cookie-parser");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 2500;
@@ -8,6 +9,7 @@ const port = process.env.PORT || 2500;
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sja1kis.mongodb.net/?retryWrites=true&w=majority`;
@@ -76,12 +78,6 @@ async function run() {
 
         // getting all books data
         app.get("/allBooks", async (req, res) => {
-            console.log("all books cookies", req.cookies);
-
-            // console.log("query mail", req.query.email);
-            // console.log("user mail", req.user.email);
-            // console.log("token owner", req.user)
-
             const cursor = addBooksData.find();
             const result = await cursor.toArray();
             res.send(result);
